1791315cc688e76b254a467c6bb13ae59d8073a2
Natasza ASzatan <natiye.nb@gmai.com>
2024-06-15 15:08:06 +0100

    tryingto initilaize the list

--- UserList.js
+++ UserList.js
@@ -2,7 +2,6 @@ export class UserList {
     constructor() {
         this.fetchedData ={}
         this.fetchData()
-        // this.initializeList()
     }
     async fetchData() {
         await Promise.all([
@@ -14,15 +13,18 @@ export class UserList {
                 Promise.all(responsesArray.map((res) => res.json())),
             )
             .then((data) => (this.fetchedData = data))
-            .then((data) => console.log(data));
+            .then((data) => console.log(data))
+            .then((this.initializeList()))
     }
-    // initializeList(){
-    //     for(let i = 0; i < this.userData.length; i ++){
-    //         const userContainer = document.createElement("div");
-    //         document.body.append(userContainer);
-    //         userContainer.classList.add('userContainer');
-    //         userContainer.innerHTML = "USERNAME"
-    //     }
-    //
-    // }
+    initializeList(){
+        const listContainer = document.querySelector('#app')
+        for(let i = 0; i < this.fetchedData.length; i ++){
+            const userContainer = document.createElement("div");
+            console.log("creating list")
+            listContainer.append(userContainer);
+            userContainer.classList.add('userContainer');
+            userContainer.innerHTML = "USERNAME"
+        }
+
+    }
 }
